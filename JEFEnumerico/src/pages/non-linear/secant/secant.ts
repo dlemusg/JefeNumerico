9af64b879978118dc60dd9a832026e526381a628
jrodri92 <jrodri92@eafit.edu.co>
2018-11-12 19:44:19 -0500

    añadiendo grafico a secante

--- JEFEnumerico/src/pages/non-linear/secant/secant.ts
+++ JEFEnumerico/src/pages/non-linear/secant/secant.ts
@@ -16,6 +16,7 @@ export class SecantPage {
   private apiUrl;
   private dataSubmit = {};
   private dataReceived = {};
+  private graf = false;
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
     public alertCtrl: AlertController, public HttpNonLinearProvider:
@@ -50,6 +51,7 @@ export class SecantPage {
   /* check if the fields are empty and show a signal, if they are empty, call 
   the postServer function */
   submitForm() {
+    this.graf = false;
     if (this.dataSubmit['f'] == '') {
       this.showAlert("ERROR:", "The field f(x) can not be empty");
     } else if (this.dataSubmit['x0'] == '') {
@@ -69,8 +71,39 @@ export class SecantPage {
 
   // add the graphing page below the buttons and hide the table.
   graficador() {
-    this.showAlert("ERROR","It has not been implemented");
-    console.log("falta implementarme");
+    this.graf = true;
+    var points = []
+    var final = []
+
+    for (var i = 0; i < this.dataReceived['x0'].length; i++) {
+
+      if (i == this.dataReceived['x0'].length - 1) {
+        final.push({
+          "x": this.dataReceived['x0'][i],
+          "y": this.dataReceived['fx'][i]
+        });
+      }
+      points.push({
+        "x": this.dataReceived['x0'][i],
+        "y": this.dataReceived['fx'][i]
+      });
+    }
+    var aux: number = <number><any>this.dataReceived['x0'][this.dataReceived['x0'].length-1];
+    var send = {
+      'f': this.dataSubmit['f'],
+      'a': this.dataSubmit['x0'],
+      'b': ""+(aux+0.5),
+      'lpoints': ["x"],
+      'lraices': ["x final"],
+      'points': points,
+      'raices': final
+    };
+    this.navCtrl.push(GraficadorPage, send);
+  }
+
+  graph() {
+    this.submitForm();
+    this.graf = true;
   }
 
   // add the graphing page below the buttons and hide the table.
@@ -123,6 +156,7 @@ export class SecantPage {
           this.showAlert("ERROR", result)
         }else {
           this.dataReceived = result;
+          if(this.graf) this.graficador();
           if(this.dataReceived['n'].length == 0 && this.dataReceived['raices'].length  == 1){
             this.showAlert("root: ",this.dataReceived["raices"][0]);
           }
