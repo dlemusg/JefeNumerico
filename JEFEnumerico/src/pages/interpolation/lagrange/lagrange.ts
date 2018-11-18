d4ee263bce171c021f15ef51bd2a9945242313c4
jrodri92 <jrodri92@eafit.edu.co>
2018-11-18 05:39:06 -0500

    terminado lagrange

--- JEFEnumerico/src/pages/interpolation/lagrange/lagrange.ts
+++ JEFEnumerico/src/pages/interpolation/lagrange/lagrange.ts
@@ -1,9 +1,9 @@
-import { GraficadorPage } from './../../graficador/graficador';
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams, AlertController }
   from 'ionic-angular';
 import { HttpNonLinearProvider }
   from './../../../providers/http-non-linear/http-non-linear';
+import { Chart } from 'chart.js';
 
 @IonicPage()
 @Component({
@@ -11,43 +11,45 @@ import { HttpNonLinearProvider }
   templateUrl: 'lagrange.html',
 })
 export class LagrangePage {
+  @ViewChild('line') line;
+  private lineChart: any;
 
-  private apiUrl  = 'https://stormy-depths-76714.herokuapp.com/lagrange';
-  
-  showResult = false;
-  //Estructura que se enviará al servidor
-  datasubmit = {
-    x : {},
-    y : {}
-  };
+  private apiUrl = 'https://stormy-depths-76714.herokuapp.com/lagrange';
 
-  //Datos recibidos por el servidor
+  private showResult = false;
+
+  private datasubmit = { x: [], y: [] ,v: ""};
+  private v = "";
+  private p = "";
+
   private dataReceived = {};
 
-  //Variables que nos ayudan a crear las entradas de usuario
-  matrix: Array<string> = [];
-  n: any;
-  input: string;
-  L = []
-  eval:any;
-  funcion:string;
-  showmatriz = false;
-  constructor(public navCtrl: NavController, public navParams: NavParams, 
-    public alertCtrl : AlertController, 
+  private matrix: Array<string> = [];
+  private n: any;
+  private input: string;
+  private L = [];
+  private eval: any;
+  private funcion: string;
+  private showmatriz = false;
+  private points = [];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    public alertCtrl: AlertController,
     public HttpNonLinearProvider: HttpNonLinearProvider) {
     this.n = '';
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad GaussSimplePage');
+    this.drawFunction({});
   }
   createMatrix() {
-    this.showmatriz =true;
+    this.showmatriz = true;
     this.showResult = false;
     this.matrix = [];
     this.datasubmit = {
-      x : {},
-      y : {}
+      x: [],
+      y: [],
+      v: ""
     };
 
     this.matrix = [];
@@ -55,12 +57,6 @@ export class LagrangePage {
     for (let i = 0; i < this.n; i++) {
       this.matrix.push(String(i));
     }
-    console.log(this.matrix);
-    console.log(this.matrix.length);
-  }
-  getN() {
-    console.log(this.n);
-    return this.n;
   }
 
   showAlert(error, subtitle) {
@@ -73,48 +69,101 @@ export class LagrangePage {
   }
 
   submitForm(){
-    console.log(this.datasubmit);
-    this.postServer();
+    if (this.datasubmit.x.length != this.n)
+      this.showAlert("ERROR:", "X's are not complete");
+    else if (this.datasubmit.y.length != this.n)
+      this.showAlert("ERROR:", "Y's are not complete");
+    else this.postServer();
   }
 
-  private help () {
+  private help() {
     let alert = this.alertCtrl.create({
       title: '¿Qué debo hacer?',
       subTitle: `<ul>
                     <li> <b> Number of Points: </ b> Number of points to be interpolated </ li>
-                    <li> <b> The point to evaluate in the function must be in the interval! </b> </li>
+                    <li> <b> The point to evaluate in the function must be in the interval </b> </li>
                   </ul> `,
       buttons: ['OK']
     });
     alert.present();
   }
-  //Zona de Get y Post
 
-  private results(){
-    if(this.dataReceived['error'] == null ){
-      this.funcion = this.dataReceived['funcion'];
-      this.eval = this.dataReceived['y_eval'];
-
-    }else{
-        this.showAlert("OJO!",this.dataReceived['error']);
-        this.funcion = this.dataReceived['funcion'];
-      }
+  private results() {
+    if (this.dataReceived['error'] == null) {
+      this.funcion = this.dataReceived['P'];
+      this.L = this.dataReceived['L'];
+      this.eval = this.dataReceived['V'];
+      this.p = "P(" + this.datasubmit['v'] + ")";
+      this.points = [];
+      for(var i = 0; i < this.datasubmit['x'].length ; i++)
+        this.points.push({
+          "x": this.datasubmit['x'][i], 
+          "y": this.datasubmit['y'][i] 
+        });
+      this.drawFunction(this.dataReceived['data']);
+    } else {
+      this.showAlert("ERROR:", this.dataReceived['error']);
+    }
   }
 
   public postServer() {
+    console.log("ENVIARE");
+    console.log(this.datasubmit)
     this.HttpNonLinearProvider.post(this.datasubmit, this.apiUrl)
-    .then(result => {
-      this.dataReceived = result;
-      this.showResult = true;
-      this.results();
-      console.log("RECIVI");
-      console.log(this.dataReceived);
-      this.eval = this.dataReceived['P'];
-      this.L =this.dataReceived['L'];
-    }, (err) => {
-      this.showAlert("ERORR:", "verify parameters entered");
-      console.log(err);
-    });
+      .then(result => {
+        console.log("RECIVI");
+        console.log(this.dataReceived);
+        this.dataReceived = result;
+        this.showResult = true;
+        this.results();
+      }, (err) => {
+        this.showAlert("ERORR:", "verify parameters entered");
+        console.log(err);
+      });
   }
 
+  drawFunction(points) {
+    this.lineChart = new Chart(this.line.nativeElement, {
+      type: 'line',
+      data: {
+        datasets: [{
+          label: ["P(x)"],
+          data: points,
+          type: 'line',
+          fill: false,
+          borderColor: [
+            '#001f51',
+          ],
+          borderWidth: 0.5
+        }, {
+          label: ["Points"],
+          type: 'bubble',
+          backgroundColor: "rgba(255,0,0,0.2)",
+          borderColor: "rgba(255,0,0,1)",
+          radius: '3',
+          data: this.points
+        }, {
+          label: this.p,
+          type: 'bubble',
+          backgroundColor: "rgba(0,255,0,0.2)",
+          borderColor: "rgba(0,255,0,1)",
+          radius: '4',
+          data: [{"x": this.datasubmit['v'], "y": this.dataReceived['V']}]
+        }],
+      },
+      options: {
+        scales: {
+          xAxes: [{
+            type: 'linear',
+            position: 'bottom'
+          }]
+        },
+        elements: {
+          point: {
+            radius: 0
+          }
+        }
+      }
+    });
+  }
 }
